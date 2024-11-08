655610931ddccc6c8237beb00276d1a31d320545
zain ahmed <zain.ahmed@dominos.com.au>
2024-11-08 23:21:21 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import React from 'react'
-import { Loader } from './loader'
+import ReactLoader from './loader'
 import './index.css'
 
-export const ReactLoader = ({ text }) => {
-  return <Loader/>
-}
+export default ReactLoader
